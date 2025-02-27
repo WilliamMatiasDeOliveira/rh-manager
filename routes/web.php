702ebe1f9c88e56d000f5117aeb4b1eac10326be
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-02-27 18:15:45 -0300

    test mailHog

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,14 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-
+Route::get('/mail', function(){
+    Mail::raw('Menssagen de teste de e-mail', function(Message $message){
+        $message->to('test@gmail.com')
+        ->subject('Bem vindo ao meu sistema')
+        ->from('rh@manager.com');
+    });
+    echo 'email enviado com sucesso';
+});
