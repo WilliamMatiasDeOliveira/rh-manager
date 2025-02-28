429b844ed536f63fc6b345af90bae80e1accfd0e
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-02-28 08:01:43 -0300

    config relations models

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
+/**
+ * TESTE FUNCINAL PARA DISPARO DE E-MAIL
+ *
+ * # Disparo do trigger adicione /mail na url
+ */
+
+// e-mail trigger using mailHog
 Route::get('/mail', function(){
     Mail::raw('Menssagen de teste de e-mail', function(Message $message){
         $message->to('test@gmail.com')
@@ -12,3 +20,22 @@ Route::get('/mail', function(){
     });
     echo 'email enviado com sucesso';
 });
+
+/**
+ * TESTE FUNCIONAL PARA A BUSCA DE UM
+ * USUÁRIO COM O ID = 1
+ *
+ * 1 - Buscar apenas o usuario
+ * 2 - buscar o usuario com os dados de details e department
+ *
+ * #  Disparo do trigger adicione /admin na url
+ */
+
+// find all user where id equal 1
+Route::get('/admin', function(){
+    $admin = User::with('detail', 'department')
+                ->find(1);
+    dd($admin->toArray());
+});
+
+
